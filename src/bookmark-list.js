cfa49cb832be333f4e95b07b457b8e5e6dc88d85
Evan Vogts <evanjvogts@gmail.com>
2019-10-02 12:06:41 -0500

    added mobile css stylings

--- src/bookmark-list.js
+++ src/bookmark-list.js
@@ -20,17 +20,17 @@ const addNewBookmarkForm = function () {
 
     <div class="bookmark-title">
         <label for="bookmark-title-entry">Title:</label>
-        <input type="text" name="bookmark-title-entry" placeholder="Bookmark Title..." required>
+        <input type="text" class="bookmark-title-entry" name="bookmark-title-entry" placeholder="Bookmark Title..." required>
     </div>
 
     <div class="bookmark-URL">
         <label for="bookmark-URL-entry">URL:</label>
-        <input type="url" name="bookmark-URL-entry" placeholder="https://NewBookmarksURL.com" required>
+        <input type="url" class="bookmark-URL-entry" name="bookmark-URL-entry" placeholder="https://NewBookmarksURL.com" required>
     </div>
 
     <div class="rating">
         <p>Rate your bookmark:</p>
-        <select id="rating" name="rating" required>
+        <select id="rating" class="ratingMenu" name="rating" required>
             <option value"5">5</option>
             <option value"4">4</option>
             <option value"3">3</option>
@@ -40,7 +40,7 @@ const addNewBookmarkForm = function () {
     </div>
 
     <div class="description">
-        <input type="text" name="desc" placeholder="Add a description for your new bookmark..." required>
+        <input type="text" class="description-box" name="desc" placeholder="Add a description for your new bookmark..." required>
     </div>
 
     <button class="submit" type="submit">Submit</button>
@@ -51,7 +51,7 @@ const generateBookmarkElement = function (bookmark) {
     if (store.expandedView === true)
     return `
     <li class="js-bookmark" data-bookmark-id="${bookmark.id}">
-    <h3><button class="toggleExpanded"><b>${bookmark.title}</b> ... Rating: ${bookmark.rating}</button></h3>
+    <h3><button class="toggleExpanded"><b>${bookmark.title}</b> ...... Rating: ${bookmark.rating}</button></h3>
     <button onclick="window.location.href='${bookmark.url}';" class="visit-URL js-visit-URL">Visit Site!</button>
     <p>Description: ${bookmark.desc}</p>
     <button class="delete">Delete</button>
@@ -59,7 +59,7 @@ const generateBookmarkElement = function (bookmark) {
     else if (store.expandedView === false)
     return `
     <li class="js-bookmark" data-bookmark-id="${bookmark.id}">
-    <h3><button class="toggleExpanded"><b>${bookmark.title}</b> ... Rating: ${bookmark.rating}</button></h3>
+    <h3><button class="toggleExpanded"><b>${bookmark.title}</b> ...... Rating: ${bookmark.rating}</button></h3>
     `;
 };
 
@@ -86,9 +86,10 @@ const handleToggleBookmarkClick = function () {
 
 const generateBookmarkList = function(bookmarks){
     const nav = `
-        <div>
-            <label for="newBookmark">Create a new bookmark</label>
-            <button class="addNewForm" name="newBookmark" type="button">+</button>
+        <div class="options">
+            <button class="addNewForm" name="newBookmark" type="button">New Bookmark +</button>
+            <div class="filter">
+            <label for="ratingMenu">Filter Bookmarks:</label>
             <select id="filter-menu" name="ratingMenu">
                 <option value="5">5</option>
                 <option value="4">4</option>
@@ -96,6 +97,7 @@ const generateBookmarkList = function(bookmarks){
                 <option value="2">2</option>
                 <option value="1">1</option>
             </select>
+            </div>
       </div>
     `;
     const items = bookmarks.map((bookmark) => generateBookmarkElement(bookmark));
